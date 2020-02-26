c6bcdb1a00dc0856af10c5b20000c09151349f49
rubemnobre <rubemjrx@hotmail.com>
2020-02-26 14:22:23 -0300

    remove files without directories

--- new.py
+++
@@ -1,8 +0,0 @@
-import time
-import struct
-import sys
-unit = sys.argv[1]
-now = time.localtime()
-arquivo = open("C:/Users/3AEMAQ21/Desktop/log.txt", "a")
-arquivo.write("%s %d/%d/%d %d:%d:%d\n" %(unit, now.tm_mday, now.tm_mon, now.tm_year, now.tm_hour, now.tm_min, now.tm_sec))
-arquivo.close()
