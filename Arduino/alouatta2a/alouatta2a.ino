6dab8e6540f302022d97ff6131b30310ca89ad27
rubemnobre <rubemjrx@hotmail.com>
2020-04-18 23:49:19 -0300

    add comments to clarify code

--- Arduino/alouatta2a/alouatta2a.ino
+++ Arduino/alouatta2a/alouatta2a.ino
@@ -1,34 +1,42 @@
+//Written by Rubem Nobre, you can find me at github.com/rubemnobre and linkedin.com/in/rubemjrx
+
 #include <TimerOne.h>
 #include <SoftwareSerial.h>
 
-int useSerial = 0;
-const int lorapin[] = {2, 3, 4, 5, 6};
-const int sendpin = 11;
+#define M0  lorapin[0]
+#define M1  lorapin[1]
+#define RX  lorapin[2]
+#define TX  lorapin[3]
+#define AUX lorapin[4]
 
-SoftwareSerial lora(lorapin[3], lorapin[2]);
+int useSerial = 0; //this variable defines if the data output goes through the R232 port or the LoRa module (it exists for debugging)
+const int lorapin[] = {2, 3, 4, 5, 6}; //these variables represent how the LoRa is wired to the microcontroller, as follows: M0 M1 RX TX AUX
 
-int lorastate;
+SoftwareSerial lora(lorapin[3], lorapin[2]); //sets up a SoftwareSerial port with the LoRa module
 
-const int samples = 512;
-const int frequency = 4000;
-uint16_t amostras[samples];
+//this sets the settings for the sound capturing
+const int bufferSize = 512; //LoRa module buffer size in bytes
+const int samples = 512; //number of samples per transmission (samples take two bytes), always a power of 2
+const int frequency = 4000; //sampling frequency
+uint16_t amostras[samples]; //sample data stack
 
 void setup() {
   useSerial = 0;
-  pinMode(lorapin[0], OUTPUT);
-  pinMode(lorapin[1], OUTPUT);
+  pinMode(M0, OUTPUT); //setting the M0 and M1 as outputs 
+  pinMode(M1, OUTPUT);
   pinMode(sendpin, INPUT_PULLUP);
-  digitalWrite(lorapin[0], LOW);
+  digitalWrite(lorapin[0], LOW); //setting the LoRa mode pins to Mode 0
   digitalWrite(lorapin[1], LOW);
-  attachInterrupt(digitalPinToInterrupt(lorapin[4]), loraevent, FALLING);
-  attachInterrupt(digitalPinToInterrupt(sendpin), sendevent, FALLING);
+  attachInterrupt(digitalPinToInterrupt(AUX), loraevent, FALLING); //assigns a function to treat an event signaled by the AUX pin
   lora.begin(9600);
   Serial.begin(9600);
-  Timer1.initialize(1e6 * ((double)1 / (double)frequency));
+  Timer1.initialize(1e6 * ((double)1 / (double)frequency)); //set the sampling function timing on microsseconds
   Timer1.attachInterrupt(sampling);
 }
 
 volatile int samplen = 0;
+
+//sampling function
 void sampling() {
   if (samplen < samples) {
     amostras[samplen] = analogRead(0);
@@ -38,46 +46,35 @@ void sampling() {
 
 void loop() {
   if (samplen > samples) {
-    Timer1.stop();
+    Timer1.stop(); //stops the timer, so no timer interrupts while this procedure is running
+
+    while (digitalRead(AUX) == LOW); //wait until the LoRa module is free to start sending data
+
     byte buf[2];
     if (useSerial == 0) {
-      for (int i = 0; i < 256; i++) {
+      for (int i = 0; i < samples; i++) {
+        //if the ammount of data to be transmitted is larger than the module buffer, wait until the end of the LoRa transmission
+        if(i != 0 && i % bufferSize/2 == 0)
+          while (digitalRead(AUX) == LOW); //the aux pin goes LOW when the module is transmitting data, so the loop will only exit when it's done
+        
+        //the write function only takes a single byte or a byte array, so this splits the uint16 to a 2-byte array and writes it
         buf[0] = amostras[i] & 255;
         buf[1] = (amostras[i] >> 8)  & 255;
-        lora.write(buf, 2);
+        lora.write(buf, 2); 
       }
-      while (digitalRead(lorapin[4]) == LOW);
-      for (int i = 256; i < 512; i++) {
-        buf[0] = amostras[i] & 255;
-        buf[1] = (amostras[i] >> 8)  & 255;
-        lora.write(buf, 2);
-      }
-      while (digitalRead(lorapin[4]) == LOW);
     } else {
+      //send debugging data in the same format so the same software can be used in the receiving end
       for (int i = 0; i < 512; i++) {
         buf[0] = amostras[i] & 255;
         buf[1] = (amostras[i] >> 8)  & 255;
         Serial.write(buf, 2);
       }
     }
-    /*
-      for (int i = 0; i < samples; i++) {
-      byte buf[2];
-      buf[0] = amostras[i] & 255;
-      buf[1] = (amostras[i] >> 8)  & 255;
-      Serial.write(buf, 2);
-      }*/
     samplen = 0;
     Timer1.start();
     interrupts();
   }
-  delay(1000);
+  delay(1000); //deliberately stop the node every cycle as a power saving measure
 }
 
 void loraevent() {}
-
-
-void sendevent() {
-  Timer1.start();
-  noInterrupts();
-}
--- Central/alouatta1f.py
+++ Central/alouatta1f.py
@@ -1,3 +1,5 @@
+#Written by Rubem Nobre, you can find me at github.com/rubemnobre and linkedin.com/in/rubemjrx
+
 import serial
 import numpy.fft
 import matplotlib.pyplot as plt
@@ -6,48 +8,24 @@ import time
 import struct
 import subprocess
 
-def readFromFile():
-	dataSource = open("alo.bin", 'rb')
-	n0 = int.from_bytes(dataSource.read(2), byteorder='little', signed=False)
-	n1 = int.from_bytes(dataSource.read(2), byteorder='little', signed=False)
-	output = []
-	for i in range(n0):
-		fmt = '>' + 'f'*n1
-		output.append(list(struct.unpack(fmt, dataSource.read(4*n1))))
-	dataSource.close()
-	return output
-
-def writeToFile(toWrite):
-	n0 = len(toWrite)
-	n1 = len(toWrite[0])
-	dataSource = open("alo.bin", 'wb')
-	dataSource.write(int.to_bytes(n0, 2, byteorder='little', signed=False))
-	dataSource.write(int.to_bytes(n1, 2, byteorder='little', signed=False))
-	for sublist in toWrite:
-		for n in sublist:
-			dataSource.write(struct.pack(">f", n))
-	dataSource.close()
-def appendToFile(toAppend):
-	read = readFromFile()
-	read.append(toAppend)
-	writeToFile(read)
-
+#reads the 512 uint16_t samples from serial (the lora module or the actual node in debugging mode) to an integer array
 def getSamplesArray():
-	#print("lendo")
 	intArray = []
 	bytes = arduino.read(1024)
-	if len(bytes) == 1024:
+	if len(bytes) == 1024: #checks if the serial buffer returned the right amount of data (it might not, if out of sync)
 		for x in range(512):
+			#the data is sent in byte pairs, so it gets converted before appending to the output array
 			pair = bytes[2*x : 2*x + 1]
-			intArray.append(int.from_bytes(pair, byteorder='little', signed=False) - 511)
-		#print("%d bytes lidos" % len(bytes))
-		arduino.reset_input_buffer()
+			intArray.append(int.from_bytes(pair, byteorder='little', signed=False) - 511) #removes the offset added by the amp before transmisson
+		arduino.reset_input_buffer() #clear the input buffer
 		return intArray
 	else:
-		arduino.reset_input_buffer()
+		arduino.reset_input_buffer() #clear the input buffer
 		return []
 
-portName = sys.argv[1]
+portName = sys.argv[1] #the serial port name is read from the cli argument
+
+#initializing the arduino Serial object
 arduino = serial.Serial(
 	port=portName,
 	baudrate=9600,
@@ -56,50 +34,56 @@ arduino = serial.Serial(
 	bytesize=serial.EIGHTBITS
 )
 
-st = 0
+st = 0 #indicates if there is data in the buffer
+
 frequencies = []
 for x in range(0, 256):
-	frequencies.append(x*2000/256)
+	frequencies.append(x*2000/256) #calculate the frequencies in the 2kHz range in the 256 steps of the output data
+
 arduino.inter_byte_timeout = 2
 arduino.reset_input_buffer()
 prevBufferSize = 0
 bufferTime = 0.0
-rst = 0
+
+rst = 0 #indicates if the timing has been reset so it does not reset repeatedly when there is no new data
 while 1:
-	if arduino.in_waiting > prevBufferSize:
+	#these checks are intended to sync the data receiving timing
+	if arduino.in_waiting > prevBufferSize: 
 		bufferTime = time.process_time()
 		rst = 0
-	if time.process_time() - bufferTime > 0.5 and rst == 0:
+	if time.process_time() - bufferTime > 0.5 and rst == 0: #resets the input buffer if the time between two bytes is > 0.5s
 		arduino.reset_input_buffer()
 		st = 0
 		print('reset because deltat is %f' %(time.process_time() - bufferTime))
 		rst = 1
-	if arduino.in_waiting > 0 and st == 0:
-		tstart = time.process_time()
-		st = 1
-	if arduino.in_waiting > 1023:
+
+#	if arduino.in_waiting > 0 and st == 0:
+#		tstart = time.process_time() #gets the time the transmission starts for debugging
+#		st = 1
+	
+	if arduino.in_waiting > 1023: 
 		sampleInput = getSamplesArray()
 		st = 0
 		if sampleInput != []:
-			deltat = time.process_time() - tstart
+			#deltat = time.process_time() - tstart #measuring the time it took to receive all the data
 			#print("%f segundos para receber" % deltat)
-			compOutput = numpy.fft.rfft(sampleInput)
+			compOutput = numpy.fft.rfft(sampleInput) #calculate the fft of the input data (time -> frequency) using NumPy
 			realOutput = []
 			for z in compOutput:
-				realOutput.append(abs(z))
-			#print("fft pronta")
+				realOutput.append(abs(z)) #converts the complex output to absolute values
+
+			#sets up a plot to show the output to the user
+			#note the first two numbers and the second half of the data is ommited because of 
+			#the limitations of this implementation of the fourier transforms
 			plt.ylabel('Magnitude')
 			plt.xlabel('Frequência')
 			plt.ylim(0, 25000)
-			plt.plot(frequencies[2:256], realOutput[2:256])
+			plt.plot(frequencies[2:256], realOutput[2:256]) #
 			if sum(realOutput[2:256])/len(realOutput[2:256]) < max(realOutput[2:256])/4:
                                val = frequencies[realOutput.index(max(realOutput[2:256]))]
                                print(val)
-                               if val > 600 and val < 1250:
-                                       print("macaco!")
-                                       subprocess.run(["python", "c://users/3aemaq21/downloads/alo/new.py", "a"])
-			#plt.show()
-			#input()
-			##if save == '1':
-			##	appendToFile(realOutput)
+                               if val > 600 and val < 1250: #checks if the amplitude peak is in the expected frequency range for the purpose of the project
+                            	       print("macaco!")
+									   #run the new.py script that logs the detection to a .txt file, the second argument indicates the data origin in the log
+                                       subprocess.run(["python", "./new.py", "musa"]) #you may have to put in the whole path to run this in windows
 	prevBufferSize = arduino.in_waiting
